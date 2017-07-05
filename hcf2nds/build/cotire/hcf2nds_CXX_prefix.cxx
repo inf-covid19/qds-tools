c12bc2fdf0d13362354e43e62a31b4de53f090a9
Cícero Pahins <cicerolp@gmail.com>
2017-07-04 21:37:32 -0300

    update hcf2nds

--- hcf2nds/build/cotire/hcf2nds_CXX_prefix.cxx
+++
@@ -1,5 +0,0 @@
-/* cotire.cmake 1.7.8 generated file */
-/* /home/cicerolp/Git/nds-tools/hcf2nds/build/cotire/hcf2nds_CXX_prefix.cxx */
-#ifdef __cplusplus
-#include "/home/cicerolp/Git/nds-tools/hcf2nds/stdafx.h"
-#endif
